e1a189fc4b43aecadd3e2d9c52b033043cb1fde0
Marcio Gimenez <marcio.gimenez@somoseducacao.com.br>
2017-07-19 22:36:47 -0300

    erro sobre 3

--- about.php
+++ about.php
@@ -15,7 +15,7 @@
                 padding-top: 10px;
             }
             .lang{
-                margin-top: 15px;
+                margin-top: 0px;
             }
         </style>
     </head>
